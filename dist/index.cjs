444ae50547ad1d0c22a4cfc3730660a0d3e6d351
Nicolas Ollier <91204591+nicolasollier@users.noreply.github.com>
2024-04-10 17:48:37 +0200

    remove default styles

--- dist/index.cjs
+++ dist/index.cjs
@@ -5,27 +5,6 @@ var React = require('react');
 const Modal = ({ title, text, onClick, customStyles }) => {
     const modalRef = React.useRef(null);
     const defaultStyles = {
-        modal: {
-            position: 'fixed',
-            top: 0,
-            left: 0,
-            width: '100vw',
-            height: '100vh',
-            display: 'flex',
-            justifyContent: 'center',
-            alignItems: 'center',
-            backgroundColor: 'rgba(0, 0, 0, 0.4)',
-        },
-        content: {
-            position: 'relative',
-            width: '60%',
-            maxWidth: '500px',
-            padding: '20px',
-            borderRadius: '5px',
-            border: '2px solid',
-            backgroundColor: 'white',
-            boxShadow: '0px 0px 10px rgba(0, 0, 0, 0.3)',
-        },
         close: {
             position: 'absolute',
             top: '6px',
@@ -35,9 +14,9 @@ const Modal = ({ title, text, onClick, customStyles }) => {
         },
     };
     const mergedStyles = {
-        modal: Object.assign(Object.assign({}, defaultStyles.modal), ((customStyles === null || customStyles === void 0 ? void 0 : customStyles.modal) || {})),
-        content: Object.assign(Object.assign({}, defaultStyles.content), ((customStyles === null || customStyles === void 0 ? void 0 : customStyles.content) || {})),
-        close: Object.assign(Object.assign({}, defaultStyles.close), ((customStyles === null || customStyles === void 0 ? void 0 : customStyles.close) || {})),
+        modal: Object.assign({}, customStyles === null || customStyles === void 0 ? void 0 : customStyles.modal),
+        content: Object.assign(Object.assign({}, customStyles === null || customStyles === void 0 ? void 0 : customStyles.content), { position: 'relative' }),
+        close: Object.assign(Object.assign({}, defaultStyles.close), customStyles === null || customStyles === void 0 ? void 0 : customStyles.close),
     };
     return (React.createElement("div", { className: "modal", style: mergedStyles.modal, ref: modalRef },
         React.createElement("div", { className: "modal-content", style: mergedStyles.content },
--- src/index.tsx
+++ src/index.tsx
@@ -15,27 +15,6 @@ const Modal: React.FC<ModalProps> = ({ title, text, onClick, customStyles }) => 
   const modalRef = useRef<HTMLDivElement>(null);
 
   const defaultStyles = {
-    modal: {
-      position: 'fixed',
-      top: 0,
-      left: 0,
-      width: '100vw',
-      height: '100vh',
-      display: 'flex',
-      justifyContent: 'center',
-      alignItems: 'center',
-      backgroundColor: 'rgba(0, 0, 0, 0.4)',
-    },
-    content: {
-      position: 'relative',
-      width: '60%',
-      maxWidth: '500px',
-      padding: '20px',
-      borderRadius: '5px',
-      border: '2px solid',
-      backgroundColor: 'white',
-      boxShadow: '0px 0px 10px rgba(0, 0, 0, 0.3)',
-    },
     close: {
       position: 'absolute',
       top: '6px',
@@ -46,9 +25,9 @@ const Modal: React.FC<ModalProps> = ({ title, text, onClick, customStyles }) => 
   };
 
   const mergedStyles = {
-    modal: { ...defaultStyles.modal, ...(customStyles?.modal || {}) },
-    content: { ...defaultStyles.content, ...(customStyles?.content || {}) },
-    close: { ...defaultStyles.close, ...(customStyles?.close || {}) },
+    modal: {...customStyles?.modal},
+    content: {...customStyles?.content, position: 'relative'},
+    close: {...defaultStyles.close, ...customStyles?.close},
   };
 
   return (
