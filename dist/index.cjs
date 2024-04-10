086a6f82375970c3209cb6ef9aead30161c41324
Nicolas Ollier <91204591+nicolasollier@users.noreply.github.com>
2024-04-10 17:02:50 +0200

    update dependencies

--- dist/index.cjs
+++ dist/index.cjs
@@ -2,18 +2,27 @@ Object.defineProperty(exports, '__esModule', { value: true });
 
 var React = require('react');
 
-const Modal = ({ title, text, status }) => {
+const Modal = ({ title, text, status, onClick }) => {
     const modalRef = React.useRef(null);
-    const closeModal = () => {
-        if (modalRef.current) {
-            modalRef.current.style.display = 'none';
+    const getStatusColor = (status) => {
+        switch (status) {
+            case 'success':
+                return '#5cb85c';
+            case 'error':
+                return '#d9534f';
+            case 'warning':
+                return '#f0ad4e';
+            case 'info':
+                return '#5bc0de';
+            default:
+                return '#333';
         }
     };
-    return (React.createElement("div", { className: "modal", style: styles.modal, ref: modalRef },
+    return (React.createElement("div", { className: "modal", style: Object.assign(Object.assign({}, styles.modal), { backgroundColor: getStatusColor(status) }), ref: modalRef },
         React.createElement("div", { className: "modal-content", style: styles.content },
-            React.createElement("span", { className: "close", style: styles.close, onClick: closeModal }, "\u00D7"),
+            React.createElement("span", { className: "close", style: styles.close, onClick: onClick }, "\u00D7"),
             React.createElement("div", null,
-                React.createElement("h2", null, title),
+                React.createElement("h2", { style: { color: getStatusColor(status) } }, title),
                 React.createElement("p", null, text)))));
 };
 const styles = {
@@ -23,7 +32,6 @@ const styles = {
         left: 0,
         width: '100vw',
         height: '100vh',
-        backgroundColor: 'rgba(0, 0, 0, 0.5)',
         display: 'flex',
         justifyContent: 'center',
         alignItems: 'center',
